c6911952b0b107cee5c64402a0bf964eb595abbd
ri-heme <72443078+ri-heme@users.noreply.github.com>
2021-04-25 00:49:20 +0200

    fix: try w/o xgb

--- src/predict.py
+++ src/predict.py
@@ -2,8 +2,6 @@ import argparse
 import pandas as pd
 from wn import Wavenet
 from dnn import DeepNeuralNetwork1
-from xgb import Xgboost
-from model import BaselineModel
 
 parser = argparse.ArgumentParser()
 parser.add_argument('--input_csv', default='input.csv')
@@ -19,14 +17,9 @@ with open(args.input_csv) as input_csv:
 # Run predictions
 y_predictions_wn = Wavenet().predict(df)
 y_predictions_dnn = DeepNeuralNetwork1().predict(df)
-y_predictions_xgb = Xgboost().predict(df)
-y_predictions_blm = BaselineModel(
-    model_file_path='src/model.pickle').predict(df)
 y_predictions = (
-    y_predictions_xgb * 0. +
     y_predictions_wn * 1. +
-    y_predictions_dnn * 0. +
-    y_predictions_blm * 0.
+    y_predictions_dnn * 0.
 )
 
 # Save predictions to file
