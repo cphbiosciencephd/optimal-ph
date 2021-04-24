7d80bab594d2bd0ec63bb00a87ecc848892907e0
mmattano <57752712+mmattano@users.noreply.github.com>
2021-04-24 23:34:07 +0200

    Ensemble test 9: 50 % xgd 25 % wn 25% dnn

--- src/predict.py
+++ src/predict.py
@@ -19,7 +19,7 @@ with open(args.input_csv) as input_csv:
 y_predictions_wn = Wavenet().predict(df)
 y_predictions_dnn = DeepNeuralNetwork1().predict(df)
 y_predictions_xgb = Xgboost().predict(df)
-y_predictions = y_predictions_xgb * 0.2 + y_predictions_wn * 0.4 + y_predictions_dnn * 0.4
+y_predictions = y_predictions_xgb * 0.5 + y_predictions_wn * 0.25 + y_predictions_dnn * 0.25
 
 # Save predictions to file
 df_predictions = pd.DataFrame({'prediction': y_predictions})
