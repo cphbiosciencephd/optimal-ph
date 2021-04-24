4f07d7b96f3363944fc14e47329e64b97ef986ea
mmattano <57752712+mmattano@users.noreply.github.com>
2021-04-24 21:59:47 +0200

    Ensemble test 5: 40 % wn 60 % dnn

--- src/predict.py
+++ src/predict.py
@@ -17,7 +17,7 @@ with open(args.input_csv) as input_csv:
 # Run predictions
 y_predictions_wn = Wavenet().predict(df)
 y_predictions_dnn = DeepNeuralNetwork1().predict(df)
-y_predictions = y_predictions_wn * 0.6 + y_predictions_dnn * 0.4
+y_predictions = y_predictions_wn * 0.4 + y_predictions_dnn * 0.6
 
 # Save predictions to file
 df_predictions = pd.DataFrame({'prediction': y_predictions})
