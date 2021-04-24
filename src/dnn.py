2ebb637011b46e72c0badcf19605ee87018ef49d
ri-heme <72443078+ri-heme@users.noreply.github.com>
2021-04-24 16:16:13 +0200

    feat: DNN model

---
+++ src/dnn.py
@@ -0,0 +1,29 @@
+from collections import Counter
+
+import numpy as np
+import pandas as pd
+from keras.models import load_model
+
+VOCAB = "GPAVLIMCFYWHKRQNEDSTBXZ"
+VOCAB_SIZE = len(VOCAB)
+
+
+class DeepNeuralNetwork1:
+    def preprocess_sequences(self, sequences):
+        lens = sequences.apply(len)
+
+        features = np.zeros((len(sequences), VOCAB_SIZE))
+
+        for i, seq in enumerate(sequences):
+            counts = dict.fromkeys(VOCAB, 0)
+            counts.update(Counter(seq))
+
+            features[i, :] = [*counts.values()]
+
+        x_data = pd.DataFrame(features, columns=list(VOCAB))
+        x_data = x_data.div(lens, axis=0)
+        return x_data
+
+    def predict(self, df_test):
+        X = self.preprocess_sequences(df_test["sequence"])
+        return load_model("dnn.h5").predict(X)
--- src/predict.py
+++ src/predict.py
@@ -1,6 +1,6 @@
 import argparse
 import pandas as pd
-from model import BaselineModel
+from dnn import DeepNeuralNetwork1
 
 parser = argparse.ArgumentParser()
 parser.add_argument('--input_csv', default='input.csv')
@@ -14,7 +14,7 @@ with open(args.input_csv) as input_csv:
     df = pd.read_csv(input_csv)
 
 # Run predictions
-y_predictions = BaselineModel(model_file_path='src/model.pickle').predict(df)
+y_predictions = DeepNeuralNetwork1().predict(df)
 
 # Save predictions to file
 df_predictions = pd.DataFrame({'prediction': y_predictions})
